12be3681970c5b845d4371d4e6e5bf8864c99350
donko1 <zalbertic@mail.ru>
2024-12-18 21:25:43 +0300

    Update settings.py

--- nordic_capitals/settings.py
+++ nordic_capitals/settings.py
@@ -3,8 +3,6 @@ Django settings for nordic_capitals project.
 """
 
 from pathlib import Path
-from datetime import timedelta
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
